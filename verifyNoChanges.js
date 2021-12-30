4f12817547913f31b968b58462d8838595e6df52
fatfisz <fatfisz@gmail.com>
2021-12-30 03:30:42 +0100

    Fix CI

---
+++ verifyNoChanges.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const simpleGit = require('simple-git');
+
+const git = simpleGit();
+
+git.status().then((status) => {
+  if (status.files.length) {
+    console.error('Changes detected:');
+    for (const { path, working_dir: workingDir } of status.files) {
+      console.error(`  ${workingDir} ${path}`);
+    }
+    process.exit(1);
+  }
+});
